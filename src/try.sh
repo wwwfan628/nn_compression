b12175ea668fcf294ac88b9b6bd8479f2b9fbbaf
yifanlu <wwwfan628@163.com>
2021-04-08 10:50:45 +0200

    add try.sh

---
+++ src/try.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL                   # mail configuration: NONE, BEGIN, END, FAIL, REQUEUE, ALL
+#SBATCH --output=./%j.out                 # where to store the output ( %j is the JOBID )
+#SBATCH --error=./%j.err                  # where to store error messages
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+
+/bin/echo Running on host: `hostname`
+/bin/echo In directory: `pwd`
+/bin/echo Starting on: `date`
+/bin/echo SLURM_JOB_ID: $SLURM_JOB_ID
+
+# exit on errors
+set -o errexit
+# execute python file
+python try.py --dataset_name=MNIST --model_name=LeNet5 --train_index
+echo finished at: `date`
+exit 0;
